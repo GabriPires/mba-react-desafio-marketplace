84b3018ae4e44807d8a350c1a6560ba82076a08b
Gabriel Pires <gabpiresdev@gmail.com>
2024-08-21 15:48:45 -0300

    chore: setup colors, fonts and assets

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 export function App() {
   return (
-    <h1>Hello world</h1>
+    <h1 className="font-title">Hello world</h1>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,40 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Poppins, sans-serif',
+        title: 'DM Sans, sans-serif',
+      },
+      colors: {
+        marketplace: {
+          orange: {
+            base: '#F24D0D',
+            dark: '#C43C08',
+          },
+          blue: {
+            light: '#D7EFF9',
+            base: '#5EC5FD',
+            dark: '#009CF0',
+          },
+          shape: {
+            white: '#FFFFFF',
+            background: '#FBF4F4',
+            shape: '#F5EAEA',
+          },
+          gray: {
+            100: '#ADADAD',
+            200: '#949494',
+            300: '#666666',
+            400: '#3D3D3D',
+            500: '#1D1D1D',
+          },
+          danger: '#DC3545',
+          success: '#28A745',
+        },
+      },
+
+    },
   },
   plugins: [],
 }

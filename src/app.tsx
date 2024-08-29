1b3ba4584ede7f1d18c5ac28c2b536a33366c45d
Gabriel Pires <gabpiresdev@gmail.com>
2024-08-29 17:11:17 -0300

    chore: setup axios and react query

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,13 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 
+import { queryClient } from './libs/react-query'
 import { router } from './router'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  )
 }

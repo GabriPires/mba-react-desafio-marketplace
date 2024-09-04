a383335c78b105cdaabee88454dc12bd9c9a3149
Gabriel Pires <gabpiresdev@gmail.com>
2024-09-04 15:52:19 -0300

    fix: style shift on product image width

--- src/pages/product-details/index.tsx
+++ src/pages/product-details/index.tsx
@@ -93,7 +93,7 @@ export function ProductDetailsPage() {
         <img
           src="https://plus.unsplash.com/premium_photo-1681449856688-2abd99ab5a73?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Foto do produto"
-          className="w-[415px] h-[340px] object-cover rounded-lg "
+          className="min-w-[415px] h-[340px] object-cover rounded-lg "
         />
         <Card className="w-full">
           <div className="flex items-center justify-between mb-6">
